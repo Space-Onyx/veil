b3b689159ea1abdb837ad1b72e294f44ca3c81e1
Neyran <maxnezerplay@gmail.com>
2026-03-28 23:10:13 +0300

    [PORT] Перенос снарядов для корабельных орудий...

---
+++ Content.Server/_Mono/Trigger/LightningOnTriggerSystem.cs
@@ -0,0 +1,27 @@
+using Content.Server.Explosion.EntitySystems;
+using Content.Server.Lightning;
+using Robust.Shared.Random;
+using Content.Shared.Trigger;
+
+namespace Content.Server._Mono.Trigger;
+
+public sealed partial class TriggerSystem : EntitySystem
+{
+    [Dependency] private readonly LightningSystem _lightning = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<LightningOnTriggerComponent, TriggerEvent>(OnTrigger);
+    }
+
+    private void OnTrigger(Entity<LightningOnTriggerComponent> ent, ref TriggerEvent args)
+    {
+        if (!_random.Prob(ent.Comp.Chance))
+            return;
+
+        _lightning.ShootRandomLightnings(ent, ent.Comp.Range, ent.Comp.Count, ent.Comp.LightningProto, ent.Comp.ArcDepth, ent.Comp.LightningEffects);
+    }
+}
