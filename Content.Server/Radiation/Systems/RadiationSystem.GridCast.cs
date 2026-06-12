7915549ee49ade2e36bf1b76b7edafda40216cca
Neyran <maxnezerplay@gmail.com>
2026-06-12 21:30:20 +0300

    [PATCH] Балансировка силы радиации.

--- Content.Server/Radiation/Systems/RadiationSystem.GridCast.cs
+++ Content.Server/Radiation/Systems/RadiationSystem.GridCast.cs
@@ -332,7 +332,7 @@ public partial class RadiationSystem
             {
                 // Treat resistance as attenuation per tile. This makes ordinary walls with
                 // resistance values of 1-2 meaningfully shield nearby rooms.
-                var passthroughRatio = MathF.Exp(-resData * dist); // <Onyx-RadTweak>
+                var passthroughRatio = MathF.Exp(-resData * dist * 0.6f); // <Onyx-RadTweak>
                 ray.Rads *= passthroughRatio;
 
                 // save data for debug
